0f62db47644f573ea3b1146d8788cad7113b3013
sjpark-08 <167855245+sjpark-08@users.noreply.github.com>
2024-06-03 01:01:05 +0900

    [Platinum III] Title: 성대나라의 물탱크, Time: 160 ms, Memory: 34232 KB -BaekjoonHub

--- 백준/Platinum/18227. 성대나라의 물탱크/성대나라의 물탱크.c
+++ 백준/Platinum/18227. 성대나라의 물탱크/성대나라의 물탱크.c
@@ -28,7 +28,7 @@ typedef struct _Node{
 Node *adj[200001];
 int s[200001], e[200001], d[200001];
 int N, C, Q, cnt;
-long long tree[800001];
+long long tree[400001];
 bool visited[200001];
 
 void Insert(Node *head, int v){
@@ -50,19 +50,16 @@ void dfs(int idx, int depth){
     e[idx] = cnt;
 }
 
-void update(int start, int end, int node, int index){
-    if(index < start || end < index) return;
-    tree[node] += 1;
-    if(start == end) return;
-    int mid = start + end >> 1;
-    update(start, mid, node << 1, index);
-    update(mid + 1, end, node << 1|1, index);
+void update(int i){
+    for(tree[i += N] += 1; i > 1; i >>= 1) tree[i >> 1] = tree[i] + tree[i ^ 1];
 }
-long long query(int start, int end, int left, int right, int node){
-    if(right < start || end < left) return 0;
-    if(left <= start && end <= right) return tree[node];
-    int mid = start + end >> 1;
-    return query(start, mid, left, right, node << 1) + query(mid + 1, end, left, right, node << 1|1);
+long long query(int l, int r){
+    long long res = 0;
+    for(l += N, r += N; l < r; l >>= 1, r >>= 1){
+        if(l & 1) res += tree[l++];
+        if(r & 1) res += tree[--r];
+    }
+    return res;
 }
 
 int main(void){
@@ -81,9 +78,9 @@ int main(void){
     Q = ReadInt();
     while(Q--){
         q = ReadInt(), a = ReadInt();
-        if(q & 1) update(1, N, 1, s[a]);
+        if(q & 1) update(s[a]);
         else{
-            long long ans = query(1, N, s[a], e[a], 1) * d[a];
+            long long ans = query(s[a], e[a] + 1) * d[a];
             printf("%lld\n", ans);
         }
     }
