01fc6aaddbaa1077ff444e4f83445f4bdc9d0f6b
sjpark-08 <167855245+sjpark-08@users.noreply.github.com>
2024-06-09 22:58:40 +0900

    [Platinum III] Title: 주식회사 승범이네, Time: 60 ms, Memory: 17596 KB -BaekjoonHub

--- 백준/Platinum/16404. 주식회사 승범이네/주식회사 승범이네.c
+++ 백준/Platinum/16404. 주식회사 승범이네/주식회사 승범이네.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#define BUFSIZE 1 << 20
+char readbuf[BUFSIZE];
+int rp = BUFSIZE;
+char ReadChar(){
+    if(rp == BUFSIZE){
+        fread(readbuf, 1, BUFSIZE, stdin);
+        rp = 0;
+    }
+    return readbuf[rp++];
+}
+int ReadInt(){
+    char c;
+    int ret = 0;
+    bool sign = false;
+    while(c < '0'){
+        if(c == '-') sign = true;
+        c = ReadChar();
+    }
+    while(c >= '0'){
+        ret = ret * 10 + (c & 0xf);
+        c = ReadChar();
+    }
+    return sign ? -ret : ret;
+}
 
 typedef struct _Node{
     int vertex;
@@ -17,12 +41,11 @@ void Insert(Node *head, int v){
     newNode->next = head->next;
     head->next = newNode;
 }
-void dfs(int x, int p){
+void dfs(int x){
     s[x] = ++cnt;
     for(Node *node = adj[x]->next; node != NULL; node = node->next){
         int nx = node->vertex;
-        if(nx != p)
-            dfs(nx, x);
+        dfs(nx);
     }
     e[x] = cnt;
 }
@@ -31,8 +54,8 @@ void update_lazy(int node, int start, int end){
     if(lazy[node] == 0) return;
     tree[node] += (end - start + 1) * lazy[node];
     if(start != end){
-        lazy[node * 2] += lazy[node];
-        lazy[node * 2 + 1] += lazy[node];
+        lazy[node << 1] += lazy[node];
+        lazy[node << 1|1] += lazy[node];
     }
     lazy[node] = 0;
 }
@@ -43,47 +66,46 @@ void update_range(int node, int start, int end, int left, int right, int dif){
     if(left <= start && end <= right){
         tree[node] += (end - start + 1) * dif;
         if(start != end){
-            lazy[node * 2] += dif;
-            lazy[node * 2 + 1] += dif;
+            lazy[node << 1] += dif;
+            lazy[node << 1|1] += dif;
         }
         return;
     }
-    int mid = (start + end) / 2;
-    update_range(node * 2, start, mid, left, right, dif);
-    update_range(node * 2 + 1, mid + 1, end, left, right, dif);
-    tree[node] = tree[node * 2] + tree[node * 2 + 1];
+    int mid = start + end >> 1;
+    update_range(node << 1, start, mid, left, right, dif);
+    update_range(node << 1|1, mid + 1, end, left, right, dif);
+    tree[node] = tree[node << 1] + tree[node << 1|1];
 }
 
 long long query(int node, int start, int end, int left, int right){
     update_lazy(node, start, end);
     if(end < left || right < start) return 0;
     if(left <= start && end <= right) return tree[node];
-    int mid = (start + end) / 2;
-    return query(node * 2, start, mid, left, right) + query(node * 2 + 1, mid + 1, end, left, right);
+    int mid = start + end >> 1;
+    return query(node << 1, start, mid, left, right) + query(node << 1|1, mid + 1, end, left, right);
 }
 
 int main(void){
     int up, q, a, b;
-    scanf("%d %d", &N, &M);
+    N = ReadInt(), M = ReadInt();
     for(int i = 0; i <= N; i++){
         adj[i] =(Node*)malloc(sizeof(Node));
         adj[i]->next = NULL;
     }
     for(int i = 1; i <= N; i++){
-        scanf("%d", &up);
+        up = ReadInt();
         if(up == -1) continue;
         Insert(adj[up], i);
-        Insert(adj[i], up);
     }
-    dfs(1, -1);
+    dfs(1);
 
     while(M--){
-        scanf("%d", &q);
+        q = ReadInt();
         if(q & 1){
-            scanf("%d %d", &a, &b);
+            a = ReadInt(), b = ReadInt();
             update_range(1, 1, N, s[a], e[a], b);
         }else{
-            scanf(" %d", &a);
+            a = ReadInt();
             long long res = query(1, 1, N, s[a], s[a]);
             printf("%lld\n", res);
         }
