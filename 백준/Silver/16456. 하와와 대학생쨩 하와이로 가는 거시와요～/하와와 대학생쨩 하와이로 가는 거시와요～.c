9056973e0af6aee30e2e227da27d835f1c023486
sjpark-08 <167855245+sjpark-08@users.noreply.github.com>
2024-06-17 00:36:14 +0900

    [Silver I] Title: 하와와 대학생쨩 하와이로 가는 거시와요~, Time: 0 ms, Memory: 1500 KB -BaekjoonHub

---
+++ 백준/Silver/16456. 하와와 대학생쨩 하와이로 가는 거시와요～/하와와 대학생쨩 하와이로 가는 거시와요～.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define MOD 1000000009
+int dp[50001][2];
+
+int main(void){
+    int N;
+    scanf("%d", &N);
+    dp[1][0] = 1, dp[1][1] = 0;
+    dp[2][0] = 1, dp[2][1] = 0;
+    for(int i = 3; i <= N; i++) {
+        dp[i][0] = (dp[i - 1][0] + dp[i - 1][1]) % MOD;
+        dp[i][1] = dp[i - 2][0];
+    }
+    printf("%d\n", (dp[N][0] + dp[N][1]) % MOD);
+}
