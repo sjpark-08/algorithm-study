8f0c42e73eb4e91bbd28abada803186f559eb6a0
sjpark-08 <167855245+sjpark-08@users.noreply.github.com>
2024-06-18 12:44:29 +0900

    [Silver I] Title: 회전 초밥, Time: 0 ms, Memory: 1260 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.c
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.c
@@ -21,7 +21,7 @@ int ReadInt(){
     return ret;
 }
 
-int map[30001];
+int map[30000];
 int check[3001];
 int N, d, k, c;
 
@@ -29,32 +29,10 @@ int main(void){
     N = ReadInt(), d = ReadInt(), k = ReadInt(), c = ReadInt();
     for(int i = 0; i < N; i++) map[i] = ReadInt();
 
-    int ans = 1;
-    int right = 0;
-    int left = 0;
-    // check[map[left]] = 1;
-    // while(left < N){
-    //     right = (right + 1) % N;
-    //     if((right - left + N) % N + 1 > k){
-    //         check[map[left]]--;
-    //         left++;
-    //     }
-    //     check[map[right]]++;
-    //     for(int i = 1; i <= d; i++) printf("%d ", check[i]); printf("\n");
-    //     if(check[map[right]] == 2){
-    //         while(left < N && check[map[right]] == 2){
-    //             check[map[left]]--;
-    //             left++;
-    //         }
-    //     }
-    //     printf("%d %d\n", left, right);
-    //     if(check[c]) ans = Max(ans, (right - left + N) % N + 1);
-    //     else ans = Max(ans, (right - left + N) % N + 1 + 1);
-    // }
     int cnt = 0;
     for(int i = 0; i < k; i++) if(check[map[i]]++ == 0) cnt++;
-    left = 0, right = k - 1;
-    ans = cnt;
+    int left = 0, right = k - 1;
+    int ans = cnt;
     if(!check[c]) ans++;
     while(left < N){
         if(--check[map[left]] == 0) cnt--;
