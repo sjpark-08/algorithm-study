0a4584c7e8c40eac50934334b1800208a775adec
sjpark-08 <167855245+sjpark-08@users.noreply.github.com>
2024-06-19 19:16:57 +0900

    [Gold V] Title: 문자열 게임 2, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.c
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
 #include <string.h>
-#include <memory.h>
 #define Min(x, y) ((x) < (y) ? (x) : (y))
 #define Max(x, y) ((x) > (y) ? (x) : (y))
-
-int check[26][10002];
-char str[10002];
 
 int main(void){
     int T, K;
     scanf("%d", &T);
     while(T--){
+        int check[26][10001];
+        char str[10001];
         scanf("%s", str);
         scanf("%d", &K);
-        
         for(int i = 0; i < 26; i++) check[i][0] = 0;
-
         for(int i = 0; str[i]; i++){
             int idx = str[i] - 'a';
             check[idx][0]++;
@@ -25,14 +21,17 @@ int main(void){
         int ans_min = 10001, ans_max = 0;
         for(int i = 0; i < 26; i++){
             if(check[i][0] < K) continue;
-            int size = check[i][0];
-            for(int j = 1; j <= size - K + 1; j++){ 
-                ans_min = Min(ans_min, check[i][j + K - 1] - check[i][j] + 1);
-                ans_max = Max(ans_max, check[i][j + K - 1] - check[i][j] + 1);
+            int left = 1;
+            int right = K;
+            while(right <= check[i][0]){
+                ans_max = Max(ans_max, check[i][right] - check[i][left] + 1);
+                ans_min = Min(ans_min, check[i][right] - check[i][left] + 1);
+                left++;
+                right++;
             }
         }
 
-        if(ans_max == 0) printf("-1\n");
+        if(!ans_max) printf("-1\n");
         else printf("%d %d\n", ans_min, ans_max);
     }
 }
