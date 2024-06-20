e48ebe0409269ec72871850a3e4714bef55b0ace
sjpark-08 <167855245+sjpark-08@users.noreply.github.com>
2024-06-20 12:38:40 +0900

    [Gold V] Title: 탑, Time: 120 ms, Memory: 7488 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdbool.h>
+#define BUFSIZE 1 << 18
+char readbuf[BUFSIZE], writebuf[BUFSIZE];
+int rp = BUFSIZE, wp = 0;
+char ReadChar(){
+    if(rp == BUFSIZE){
+        fread(readbuf, 1, BUFSIZE, stdin);
+        rp = 0;
+    }
+    return readbuf[rp++];
+}
+int ReadInt(){
+    char c;
+    int ret = 0;
+    while(c < '0') c = ReadChar();
+    while(c >= '0'){
+        ret = ret * 10 + (c & 0xf);
+        c = ReadChar();
+    }
+    return ret;
+}
+void WriteInt(int n){
+    int tmp = n;
+    int cnt = 0;
+    if(n == 0){
+        if(wp + 2 >= BUFSIZE){
+            fwrite(writebuf, 1, wp, stdout);
+            wp = 0;
+        }
+        writebuf[wp++] = 48;
+        writebuf[wp++] = ' ';
+        return;
+    }
+    while(tmp > 0){
+        cnt++;
+        tmp /= 10;
+    }
+    if(wp + cnt >= BUFSIZE){
+        fwrite(writebuf, 1, wp, stdout);
+        wp = 0;
+    }
+    writebuf[wp + cnt] = ' ';
+    int len = cnt;
+    while(n){
+        cnt--;
+        writebuf[wp + cnt] = (n % 10) + 48;
+        n /= 10;
+    }
+    wp += len + 1;
+}
+
+int stack[500002];
+int tower[500002];
+int result[500002];
+int top = -1;
+
+void push(int data){
+    stack[++top] = data;
+}
+int pop(){
+    return stack[top--];
+}
+
+int main(void){
+    int N, val;
+    tower[0] = 0x7f7f7f7f;
+    push(0);
+    // N = ReadInt();
+    scanf("%d", &N);
+    for(int i = 1; i <= N; i++) scanf("%d", &tower[i]); //tower[i] = ReadInt();
+    for(int i = 1; i <= N; i++){
+        while(tower[stack[top]] < tower[i]){
+            // int out = pop();
+            // result[out] = stack[top];
+            pop();
+        }
+        printf("%d ", stack[top]);
+        push(i);
+    }
+    // while(top > -1){
+    //     int out = pop();
+    //     result[out] = stack[top];
+    // }
+
+    // for(int i = 1; i <= N; i++) printf("%d ", result[i]);
+
+    // fwrite(writebuf, 1, wp, stdout);
+    // printf("\n");
+}
