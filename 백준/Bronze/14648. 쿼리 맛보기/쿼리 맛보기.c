a5d1bff0fadabec6df582b1459a58e8bc345ab61
sjpark-08 <167855245+sjpark-08@users.noreply.github.com>
2024-07-04 12:20:03 +0900

    [Bronze II] Title: 쿼리 맛보기, Time: 4 ms, Memory: 1152 KB -BaekjoonHub

--- 백준/Bronze/14648. 쿼리 맛보기/쿼리 맛보기.c
+++ 백준/Bronze/14648. 쿼리 맛보기/쿼리 맛보기.c
@@ -1,15 +1,40 @@
 #include <stdio.h>
+#include <stdbool.h>
+#define BUFSIZE 1 << 15
+char readbuf[BUFSIZE];
+int rp = BUFSIZE;
+char ReadChar(){
+    if(rp == BUFSIZE){
+        fread(readbuf, 1, BUFSIZE, stdin);
+        rp = 0;
+    }
+    return readbuf[rp++];
+}
+int ReadInt(){
+    char c;
+    int ret = 0;
+    bool sign = false;
+    while(c < '0' || c > '9'){
+        if(c == '-') sign = true;
+        c = ReadChar();
+    }
+    while(c >= '0' && c <= '9'){
+        ret = ret * 10 + (c & 0xf);
+        c = ReadChar();
+    }
+    return sign ? -ret : ret;
+}
 int arr[1001];
 int N, Q;
 int main(void){
     int q, a, b, c, d;
-    scanf("%d %d", &N, &Q);
-    for(int i = 1; i <= N; i++) scanf("%d", &arr[i]);
+    N = ReadInt(), Q = ReadInt();
+    for(int i = 1; i <= N; i++) arr[i] = ReadInt();
 
     while(Q--){
-        scanf("%d", &q);
+        q = ReadInt();
         if(q & 1){
-            scanf("%d %d", &a, &b);
+            a = ReadInt(), b = ReadInt();
             long long sum = 0;
             for(int i = a; i <= b; i++) sum += arr[i];
             printf("%lld\n", sum);
@@ -17,7 +42,8 @@ int main(void){
             arr[a] = arr[b];
             arr[b] = tmp;
         }else{
-            scanf("%d %d %d %d", &a, &b, &c, &d);
+            a = ReadInt(), b = ReadInt();
+            c = ReadInt(), d = ReadInt();
             long long sum1 = 0;
             for(int i = a; i <= b; i++) sum1 += arr[i];
             long long sum2 = 0;
