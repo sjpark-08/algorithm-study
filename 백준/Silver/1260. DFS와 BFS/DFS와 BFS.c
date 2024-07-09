ad5f613e093af841437393376edd772ffe72cdeb
sjpark-08 <167855245+sjpark-08@users.noreply.github.com>
2024-07-10 01:32:07 +0900

    [Silver II] Title: DFS와 BFS, Time: 0 ms, Memory: 2164 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.c
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.c
@@ -1,70 +1,78 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <memory.h>
+#define BUFSIZE 1 << 15
+char readbuf[BUFSIZE];
+int rp = BUFSIZE;
+char ReadChar(){
+    if(rp == BUFSIZE){
+        fread(readbuf, 1, BUFSIZE, stdin);
+        rp = 0;
+    }
+    return readbuf[rp++];
+}
+int ReadInt(){
+    char c;
+    int ret = 0;
+    while(c < '0' || c > '9') c = ReadChar();
+    while(c >= '0' && c <= '9'){
+        ret = ret * 10 + (c & 0xf);
+        c = ReadChar();
+    }
+    return ret;
+}
 
-int graph[1001][1001] = {0};
-int visited[1001] = {0};
-int check[1001] = {0};
-int result[1001] = {0};
+bool visited[1001], graph[1001][1001];
+int queue[10001];
+int front = -1, rear = -1;
+int N, M, V, a, b;
 
-int queue[100000] = {0};
-int front = -1;
-int rear = -1;
-
-void push(int item){
-    queue[++rear] = item;
+void push(int data){
+    rear = (rear + 1) % 10001;
+    queue[rear] = data;
 }
 int pop(){
-    return queue[++front];
+    front = (front + 1) % 10001;
+    return queue[front];
 }
 
-void DFS(int depth, int n, int v){
-    int i;
-    if(depth == n){
-        return;
-    }
-    else if(depth == 0){
-        result[depth] = v;
-        printf("%d ", v);
-        check[v] = 1;
-        DFS(depth + 1, n, v);
-    }
-    else{
-        for(i = 1; i <= n; i++){
-            if(!check[i] && graph[result[depth - 1]][i]){
-                result[depth] = i;
-                printf("%d ", i);
-                check[i] = 1;
-                DFS(depth + 1, n, v);
-            }
-        }
+void DFS(int depth, int now){
+    if(depth == N) return;
+    visited[now] = true;
+    printf("%d ", now);
+    for(int i = 1; i <= N; i++){
+        int next = i;
+        if(!graph[now][next] || visited[next]) continue;
+        DFS(depth + 1, next);
     }
 }
-
-void BFS(int n, int v){
-    visited[v] = 1;
-    printf("%d ", v);
-    push(v);
+void BFS(int src){
+    push(src);
 
     while(front != rear){
-        int vertex = pop();
-        for(int i = 1; i <= n; i++){
-            if(!visited[i] && graph[vertex][i]){
-                push(i);
-                printf("%d ", i);
-                visited[i] = 1;
-            }
+        int now = pop();
+        if(visited[now]) continue;
+        visited[now] = true;
+        printf("%d ", now);
+        for(int i = 1; i <= N; i++){
+            int next = i;
+            if(!graph[now][next] || visited[next]) continue;
+            push(next);
         }
     }
 }
 
 int main(void){
-    int n, m, v;
-    scanf("%d %d %d", &n, &m, &v);
-    int x, y;
-    for(int i = 0; i < m; i++){
-        scanf("%d %d", &x, &y);
-        graph[x][y] = graph[y][x] = 1;
+    N = ReadInt(), M = ReadInt(), V = ReadInt();
+    while(M--){
+        a = ReadInt(), b = ReadInt();
+        graph[a][b] = true;
+        graph[b][a] = true;
     }
-    DFS(0, n, v);
+
+    DFS(0, V);
+    memset(visited, false, sizeof(visited));
     printf("\n");
-    BFS(n, v);
+    BFS(V);
 }
