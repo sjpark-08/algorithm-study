95055649463c3d164b81ecd2efa67e212de6682b
sjpark-08 <167855245+sjpark-08@users.noreply.github.com>
2024-07-30 12:26:14 +0900

    [Silver III] Title: 마라톤 1, Time: 4 ms, Memory: 3696 KB -BaekjoonHub

--- 백준/Silver/10655. 마라톤 1/마라톤 1.cc
+++ 백준/Silver/10655. 마라톤 1/마라톤 1.cc
@@ -1,17 +1,45 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
+#include <cstdio>
 using namespace std;
+#define BUFSIZE 1 << 17
+char readbuf[BUFSIZE];
+char read(){
+    static int rp = BUFSIZE, size = BUFSIZE;
+    if(rp == size){
+        size = fread(readbuf, 1, BUFSIZE, stdin);
+        if(!size) return 0;
+        rp = 0;
+    }
+    return readbuf[rp++];
+}
+int readInt(){
+    int ret = 0;
+    int flag = 1;
+    char c;
+    while(c < '0' || c > '9'){
+        if(c == '-') flag = -1;
+        c = read();
+    }
+    while(c & 0x10){
+        ret = ret * 10 + (c & 0xf);
+        c = read();
+    }
+    return ret * flag;
+}
 
 vector<pair<int, int>> cp;
 
 int main(void){
-    ios_base::sync_with_stdio(false);
+    // ios_base::sync_with_stdio(false);
     int N, ans = 0;
-    cin >> N;
+    // cin >> N;
+    N = readInt();
     for(int i = 0; i < N; i++){
         int x, y;
-        cin >> x >> y;
+        // cin >> x >> y;
+        x = readInt(), y = readInt();
         cp.push_back(make_pair(x, y));
         if(i == 0) continue;
         ans += abs(cp[i].first - cp[i - 1].first) + abs(cp[i].second - cp[i - 1].second);
