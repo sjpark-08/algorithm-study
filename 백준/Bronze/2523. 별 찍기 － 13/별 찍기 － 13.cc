474022689b2c5b90d46d1f960bde1e896dabad12
sjpark-08 <167855245+sjpark-08@users.noreply.github.com>
2024-08-11 12:34:23 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int N;
+    cin >> N;
+    int idx = 0;
+    for(int i = 1; i <= 2 * N - 1; i++){
+        int K = i >= N ? N - idx++ : i;
+        int M = N - K;
+        while(K--) cout << '*';
+        // while(M--) cout << ' ';
+        cout << '\n';
+    }
+}
