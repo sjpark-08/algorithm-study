035fbb7c0cdd7206e592110f98993ea0d18fabf1
sjpark-08 <167855245+sjpark-08@users.noreply.github.com>
2024-08-16 11:22:38 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 92 ms, Memory: 17580 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.cc
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.cc
@@ -4,9 +4,28 @@
 #include <vector>
 #include <unordered_map>
 using namespace std;
+#define BUFSIZE 1 << 17
+char readbuf[BUFSIZE];
+int rp = BUFSIZE;
+char ReadChar(){
+    if(rp == BUFSIZE){
+        fread(readbuf, 1, BUFSIZE, stdin);
+        rp = 0;
+    }
+    return readbuf[rp++];
+}
+int ReadInt(){
+    int ret = 0;
+    char c;
+    while(c < '0' || c > '9') c = ReadChar();
+    while(c >= '0' && c <= '9'){
+        ret = ret * 10 + (c & 0xf);
+        c = ReadChar();
+    }
+    return ret;
+}
 
 bool compare(pair<string, int> a, pair<string, int> b){
-
     if(a.second == b.second && a.first.size() == b.first.size()) return a.first < b.first;
     else if(a.second == b.second) return a.first.size() > b.first.size();
     return a.second > b.second;
@@ -16,15 +35,16 @@ int main(void){
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
     int N, M;
-    string word;
+    char c;
     unordered_map<string, int> dict;
-    cin >> N >> M;
+    N = ReadInt(), M = ReadInt();
     for(int i = 0; i < N; i++){
-        cin >> word;
+        int k = 0;
+        string word;
+        while((c = ReadChar()) != '\n') word.push_back(c);
         if(word.size() < M) continue;
         dict[word]++;
     } 
-
     vector<pair<string, int>> ans;
     for(auto word : dict) ans.push_back(word);
     sort(ans.begin(), ans.end(), compare);
