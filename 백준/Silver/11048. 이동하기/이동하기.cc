323ea5c25a6755973b9ffbdc3f2511ab8f66866f
sjpark-08 <167855245+sjpark-08@users.noreply.github.com>
2024-08-25 14:54:24 +0900

    [Silver II] Title: 이동하기, Time: 60 ms, Memory: 10360 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.cc
+++ 백준/Silver/11048. 이동하기/이동하기.cc
@@ -1,6 +1,27 @@
 #include <iostream>
 #include <cstdio>
 using namespace std;
+#define BUFSIZE 1 << 19
+char readbuf[BUFSIZE];
+int rp = BUFSIZE;
+char read(){
+    if(rp == BUFSIZE){
+        fread(readbuf, 1, BUFSIZE, stdin);
+        rp = 0;
+    }
+    return readbuf[rp++];
+}
+int readInt(){
+    char c;
+    int ret = 0;
+    while(c < '0' || c > '9') c = read();
+    while(c >= '0' && c <= '9'){
+        ret = ret * 10 + (c & 0xf);
+        c = read();
+    }
+    return ret;
+}
+
 int map[1001][1001];
 int dp[1001][1001];
 
