60329a0b64dab9ae3703fdde652ac6939db66fd1
sjpark-08 <167855245+sjpark-08@users.noreply.github.com>
2024-08-26 13:34:14 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1120 ms, Memory: 3204 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -0,0 +1,57 @@
+#include <cstdio>
+using namespace std;
+#define BUFSIZE 1 << 20
+char readbuf[BUFSIZE], writebuf[BUFSIZE];
+int rp = BUFSIZE, wp = 0;
+char read(){
+    if(rp == BUFSIZE){
+        fread(readbuf, 1, BUFSIZE, stdin);
+        rp = 0;
+    }
+    return readbuf[rp++];
+}
+int readInt(){
+    char c;
+    int ret = 0;
+    while(c < '0' || c > '9') c = read();
+    while(c >= '0' && c <= '9'){
+        ret = ret * 10 + (c & 0xf);
+        c = read();
+    }
+    return ret;
+}
+void writeInt(int n){
+    int tmp = n, cnt = 0;
+    while(tmp > 0){
+        cnt++;
+        tmp /= 10;
+    }
+    if(wp + cnt + 1 >= BUFSIZE){
+        fwrite(writebuf, 1, wp, stdout);
+        wp = 0;
+    }
+    writebuf[wp + cnt] = '\n';
+    int len = cnt;
+    while(n > 0){
+        cnt--;
+        writebuf[wp + cnt] = n % 10 + 48;
+        n /= 10;
+    }
+    wp += len + 1;
+}
+
+int cnt[10001];
+
+int main(void){
+    int N, num;
+    N = readInt();
+    for(int i = 0; i < N; i++){
+        num = readInt();
+        cnt[num]++;
+    }
+    for(int i = 1; i <= 10000; i++){
+        if(!cnt[i]) continue;
+        for(int k = 0; k < cnt[i]; k++) printf("%d\n", i);
+    }
+    fwrite(writebuf, 1, wp, stdout);
+}
