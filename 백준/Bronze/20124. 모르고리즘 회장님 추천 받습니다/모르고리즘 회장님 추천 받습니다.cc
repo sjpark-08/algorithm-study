265bcc7d14c3491b320b6b61e5169f49fb94878d
sjpark-08 <167855245+sjpark-08@users.noreply.github.com>
2024-08-28 11:41:04 +0900

    [Bronze II] Title: 모르고리즘 회장님 추천 받습니다, Time: 16 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/20124. 모르고리즘 회장님 추천 받습니다/모르고리즘 회장님 추천 받습니다.cc
+++ 백준/Bronze/20124. 모르고리즘 회장님 추천 받습니다/모르고리즘 회장님 추천 받습니다.cc
@@ -1,25 +1,23 @@
 #include <iostream>
 #include <string>
-#include <vector>
-#include <algorithm>
 using namespace std;
-
-bool compare(pair<int, string> a, pair<int, string> b){
-    if(a.first == b.first) return a.second < b.second;
-    return a.first > b.first;
-}
 
 int main(void){
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
     int N, score;
     string name;
-    vector<pair<int, string>> cands;
     cin >> N;
+    int ansV = 0;
+    string ansN;
     for(int i = 0; i < N; i++){
         cin >> name >> score;
-        cands.push_back({score, name});
+        if(ansV < score){
+            ansV = score;
+            ansN = name;
+        }else if(ansV == score && ansN > name){
+            ansN = name;
+        }
     }
-    sort(cands.begin(), cands.end(), compare);
-    cout << cands[0].second;
+    cout << ansN;
 }
