a6a327698890809762360b5dc833b731a7260f93
sjpark-08 <167855245+sjpark-08@users.noreply.github.com>
2024-09-16 19:55:48 +0900

    [Gold IV] Title: 주난의 난(難), Time: 0 ms, Memory: 2256 KB -BaekjoonHub

--- 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.cc
+++ 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.cc
@@ -1,31 +1,8 @@
 #include <iostream>
 #include <queue>
-#include <cstdio>
 using namespace std;
-#define BUFSIZE 1 << 16
-char readbuf[BUFSIZE];
-int rp = BUFSIZE;
-char read(){
-    if(rp == BUFSIZE){
-        fread(readbuf, 1, BUFSIZE, stdin);
-        rp = 0;
-    }
-    return readbuf[rp++];
-}
-char readInt(){
-    char c;
-    int ret = 0;
-    while(c < '0' || c > '9') c = read();
-    while(c >= '0' && c <= '9'){
-        ret = ret * 10 + (c & 0xf);
-        c = read();
-    }
-    return ret;
-}
-
 
 int N, M, srcX, srcY, destX, destY;
-// char map[301][301];
 string map[301];
 bool visited[301][301];
 int dx[4] = {0, 0, -1, 1};
@@ -48,28 +25,17 @@ int bfs(){
             if(map[nx][ny] == '0') q.push_front({{nx, ny}, t});
             else q.push_back({{nx, ny}, t + 1});
             visited[nx][ny] = true;
-            // if(map[nx][ny] == '#') cout << nx << ' ' << ny << '\n';
         }
     }
     return 0;
 }
 
 int main(void){
-    // N = readInt(), M = readInt();
-    // srcX = readInt(), srcY = readInt();
-    // destX = readInt(), destY = readInt();
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
     cin >> N >> M;
     cin >> srcX >> srcY >> destX >> destY;
     srcX--, srcY--, destX--, destY--;
-    for(int i = 0; i < N; i++){
-        // for(int j = 0; j < M; j++){
-            // map[i][j] = read();
-            cin >> map[i];
-        // }
-        // read(); // LF
-    }
+    for(int i = 0; i < N; i++) cin >> map[i];
     cout << bfs();
-    // for(int i = 0; i < N; i++) cout << map[i] << '\n';
 }
