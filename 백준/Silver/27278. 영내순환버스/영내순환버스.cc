3f41b2cfac9b05d240dc5ac629d3043e12f11d37
sjpark-08 <167855245+sjpark-08@users.noreply.github.com>
2024-09-29 10:43:50 +0900

    [Silver I] Title: 영내순환버스, Time: 8 ms, Memory: 2668 KB -BaekjoonHub

--- 백준/Silver/27278. 영내순환버스/영내순환버스.cc
+++ 백준/Silver/27278. 영내순환버스/영내순환버스.cc
@@ -1,19 +1,35 @@
+#include <cstdio>
 #include <iostream>
 using namespace std;
+#define BUFSIZE 1 << 18
+char readbuf[BUFSIZE];
+int rp = BUFSIZE;
+char read(){
+    if(rp == BUFSIZE){
+        fread(readbuf, 1, BUFSIZE, stdin);
+        rp = 0;
+    }
+    return readbuf[rp++];
+}
+int readInt(){
+    char c;
+    int ret = 0;
+    while(c < '0' || c > '9') c = read();
+    while(c >= '0' && c <= '9'){
+        ret = ret * 10 + (c & 0xf);
+        c = read();
+    }
+    return ret;
+}
 
 int tsum[100001];
 int N, M, t, in, out, maxt, st;
 
 int main(void){
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cin >> N >> M;
-    for(int i = 1; i <= N; i++){
-        cin >> t;
-        tsum[i] = tsum[i - 1] + t;
-    }
+    N = readInt(), M = readInt();
+    for(int i = 1; i <= N; i++) tsum[i] = tsum[i - 1] + readInt();
     for(int i = 0; i < M; i++){
-        cin >> in >> out >> st;
+        in = readInt(), out = readInt(), st = readInt();
         int tmp = st / tsum[N];
         int start, end;
         if(tmp * tsum[N] + tsum[in - 1] >= st) start = tmp * tsum[N] + tsum[in - 1];
