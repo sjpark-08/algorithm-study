ef5f37ab5b503eb8613f6578a7ea7ee43c64ea85
sjpark-08 <167855245+sjpark-08@users.noreply.github.com>
2024-10-01 23:15:53 +0900

    [Bronze III] Title: 숫자 맞추기 게임, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4892. 숫자 맞추기 게임/숫자 맞추기 게임.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void){
+    int n0, n1, n2, n3, n4, i = 0;
+    while(++i){
+        string oddOrEven;
+        cin >> n0;
+        if(!n0) break;
+        n1 = 3 * n0;
+        if(n1 & 1){
+            n2 = n1 + 1 >> 1;
+            oddOrEven = "odd";
+        }else{
+            n2 = n1 >> 1;
+            oddOrEven = "even";
+        }
+        n3 = 3 * n2;
+        n4 = n3 / 9;
+        cout << i << ". " << oddOrEven << " " << n4 << '\n';
+    }
+}
